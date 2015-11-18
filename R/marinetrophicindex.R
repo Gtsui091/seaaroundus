4a57dcddfaba001b124ace379c257f774112784c
Robert Reis <T-RobertR@vulcan.com>
2015-11-18 14:31:30 -0800

    get rmti working

--- R/marinetrophicindex.R
+++ R/marinetrophicindex.R
@@ -20,14 +20,23 @@ marinetrophicindex <- function(region, id, chart=FALSE, type="mean_trophic_level
 
   # call API
   querystring <- paste("?region_id=", id, "&transfer_efficiency=", transferefficiency, sep="")
-  # TODO rmti has 3 data sets
   data <- callapi(paste(getapibaseurl(), region, "marine-trophic-index", querystring, sep="/"))
 
   # get the data for the chart
   named_data <- data[[1]]
   names(named_data) <- data[[2]]
-  data <- named_data[[type]]
-  df <- data.frame(level=data[,2], row.names=data[,1])
+
+  # rmti has 3 data sets
+  if (type == "rmti") {
+    df <- data.frame(level_1950=named_data[["RMTI_1950"]][,2],
+                     level_1962=named_data[["RMTI_1962"]][,2],
+                     level_1972=named_data[["RMTI_1972"]][,2],
+                     row.names=named_data[["RMTI_1950"]][,1])
+
+  } else {
+    data <- named_data[[type]]
+    df <- data.frame(level=data[,2], row.names=data[,1])
+  }
 
   # return dataframe
   if (!chart) {
@@ -35,20 +44,35 @@ marinetrophicindex <- function(region, id, chart=FALSE, type="mean_trophic_level
 
   # return chart
   } else {
-    # TODO rmti has 3 data sets
     ylabel <- switch(type,
                      mean_trophic_level = "Trophic level of the catch",
                      fib_index = "Fishing-in-balance index",
                      rmti = "RMTI")
+
     title <- switch(type,
                     mean_trophic_level = "Marine Trophic Index",
                     fib_index = "Fishing in Balance Index",
                     rmti = "Region-based Marine Trophic Index")
-    df <- data.frame(year=as.integer(unlist(rownames(df))), level=df[,1])
-    graph <- ggplot(data=df, aes(x=year, y=level)) +
-      scale_x_continuous(breaks=seq(min(df[["year"]]), max(df[["year"]]), 10), expand=c(0, 0)) +
-      geom_line(colour="blue") + geom_point(colour="blue") +
-      xlab("Year") + ylab(ylabel) + ggtitle(title)
+
+    years <- as.integer(unlist(rownames(df)))
+    df[["year"]] <- years
+
+    # rmti has 3 data sets
+    if (type == "rmti") {
+      graph <- ggplot(data=df, aes(x=year)) +
+        geom_path(aes(y=level_1950, colour="blue")) +
+        geom_path(aes(y=level_1962, colour="cyan"), na.rm=TRUE) +
+        geom_path(aes(y=level_1972, colour="red"), na.rm=TRUE)
+
+    } else {
+      graph <- ggplot(data=df, aes(x=year, y=level)) +
+        geom_path(colour="blue") + geom_point(colour="blue")
+    }
+
+    graph <- graph +
+      scale_x_continuous(breaks=seq(min(years), max(years), 10), expand=c(0, 0)) +
+      xlab("Year") + ylab(ylabel) + ggtitle(title) + theme(legend.position="none")
+
     return(graph)
   }
 }
