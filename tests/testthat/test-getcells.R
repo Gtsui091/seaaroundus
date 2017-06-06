7dd6448af2340311a1202c00f6650498770556b7
Scott Chamberlain <myrmecocystus@gmail.com>
2017-06-06 09:00:13 -0700

    need wicket, and Remotes for installation add getcells() test for bad WKT

--- tests/testthat/test-getcells.R
+++ tests/testthat/test-getcells.R
@@ -16,4 +16,13 @@ test_that("getcells works", {
 
 test_that("getcells fails well", {
   expect_error(getcells(), "argument \"shape\" is missing")
+
+  expect_error(
+    getcells("POLYGON ((-48.177685950413291 15.842380165289299,
+             -48.177685950413291 15.842380165289299,
+             -54.964876033057919 28.964280991735578,
+             -35.960743801652967 27.606842975206646,
+             -48.177685950413291 15.8423801652892))"),
+    "The WKT object is closed but does not have matching start/end points"
+  )
 })
