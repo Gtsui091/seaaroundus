ddafd5c211ef303ee963a00eac52ba3a49cb7c5f
Scott Chamberlain <myrmecocystus@gmail.com>
2017-06-14 15:33:50 -0700

    started man file for listing regions options, not done yet

---
+++ R/regions.R
@@ -0,0 +1,16 @@
+#' Regions details
+#'
+#' @name regions
+#' @details Region options
+#' \itemize{
+#'  \item eez
+#'  \item lme
+#'  \item rfmo
+#'  \item highseas
+#'  \item fao
+#'  \item eez-bordering
+#'  \item fishing-entity
+#'  \item taxon
+#'  \item global
+#' }
+NULL
